496746a224c104407055749ed5589fa1540ad873
sksasi5577 <sksasi5577@gmail.com>
2022-12-20 12:12:19 +0530

    Changes ; Login UI Updated

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home, Login } from "./containers";
+
+const App = () => {
+  return (
+    <div className="w-screen min-h-screen ">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
